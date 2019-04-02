a478e6386fae8ff62a912dd1fcd8cc003953802f
李祖明 <solace.lee@outlook.com>
2019-04-02 17:58:19 +0800

    熟悉语法

---
+++ src/components/home/showAddress.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="show-address">
+    <div class="city_name">{{cityName}}</div>
+    <div class="district_name">{{districtName}}</div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+export default class showAddress extends Vue {
+  cityName:string = ''
+  districtName:string = ''
+  created () :void {
+    
+  } 
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
