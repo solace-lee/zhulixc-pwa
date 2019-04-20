25e2f700a4504c61e118193e80d7530cc58b840d
李祖明 <solace.lee@outlook.com>
2019-04-20 08:19:28 +0800

    会员专区完成

--- src/config/apis.ts
+++ src/config/apis.ts
@@ -5,6 +5,7 @@ const apis = {
   favorite: 'commodity/indexlike', // 猜你喜欢
   greenlife: 'commodity/findByClassifyCode', // 绿色生活数据（生态市场）
   speciality: 'commodity/onetambononeproduct', // 一村一品（热销乡味）
+  member: 'commodity/findmembersarea', // 会员商品
   codeRegistered: 'sms/sendCodeRegistered', // 注册-获取验证码
   register: 'register' // 注册-提交
 }
